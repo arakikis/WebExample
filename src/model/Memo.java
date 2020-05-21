d103a77203ef7841b76020727413dbfdcbc38f77
edu06 <edu06@EDUPC06.n00d01.kis.co.jp>
2020-05-21 16:11:51 +0900

    5.21効果測定

---
+++ src/model/Memo.java
@@ -0,0 +1,42 @@
+package model;
+
+public class Memo {
+	private int mid;
+	private String title;
+	private String body;
+
+	public Memo() {
+
+	}
+
+	public int getMid() {
+		return mid;
+	}
+
+	public void setMid(int mid) {
+		this.mid = mid;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	public Memo(int mid, String title, String body) {
+		this.mid = mid;
+		this.title = title;
+		this.body = body;
+	}
+}
+
